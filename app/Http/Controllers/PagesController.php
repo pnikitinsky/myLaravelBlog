a4cdf00507eb206758608da6ac9d424c72c3fa6d
pavel <pavel>
2017-03-19 21:37:06 +0200

    first

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Routing\Controller;
+
+class PagesController extends Controller {
+    public function getIndex() {
+        return view("pages.welcome");
+    }
+
+    public function getAbout() {
+        $first = 'Pavel';
+        $last  = 'N.';
+        $fullname = $first . " " . $last;
+        $email = "pavelN@gmail.com";
+        $data['email'] = $email;
+        $data['fullname'] = $fullname;
+        return view("pages.about")->withData($data);
+    }
+
+    public function getContact() {
+        return view("pages.contact");
+    }
+
+//    public function postContact() {
+//
+//    }
+
+}
